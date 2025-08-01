564ea6fb0e558810efaf434600b56b5cb5ae7c0d
dhruv <dhruvlgd44@gmail.com>
2025-08-01 20:20:43 +0530

    new game

--- game1/scripts/coin.gd
+++ game1/scripts/coin.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
+@onready var gamemanager: Node = %Gamemanager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
-func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin")
-	queue_free()
+
+func _on_body_entered(body):
+	gamemanager.add_point()
+	animation_player.play("pickup")
+
+	
